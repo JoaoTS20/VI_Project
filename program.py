273d81c93b783de859a57720b12bf98c9072706f
pmasilva20 <pmasilva20@ua.pt>
2022-01-09 18:56:41 +0000

    Adapted to final website

--- program.py
+++ program.py
@@ -4,19 +4,25 @@ def parserTSVDict(document):
     file= open(document, encoding='utf-8', mode='rt')
     return csv.DictReader(file, delimiter=",")
 
+def no0vudget():
+        file=parserTSVDict("newdataset.csv")
+        rows=[]
+        fieldnames=["id","budget","genres","original_language","original_title","overview","popularity","poster_path","production_companies","production_countries","release_date","revenue","spoken_languages","title","director"]
+        s={}
+        d=[]
+        for row in file:
+            for a in eval(row["production_countries"]):
+                d.append(a)
+        for x in sorted(set(d)):
+            print(x)
+
 
 def main():
-        file=parserTSVDict("dataset.csv")
-        rows=[]
-        fieldnames=["id","budget","genres","original_language","original_title","overview","popularity","poster_path","production_companies","production_countries","release_date","revenue","spoken_languages","title","director"
-]
-        for row in file:
-            if float(row["budget"]) > 0:
-                rows.append(row)
-        with open("newcsv.csv","w", encoding='UTF8', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(rows)
-
+    file=parserTSVDict("dataset.csv")
+    n=[]
+    for row in file:
+        n.append(eval(row["director"]))
+    z={director for group in n for director in group}
+    print(z)
 if __name__ == "__main__":
     main()
